18f311bc6e212d6a75d9893c29c510e88dbdcd2e
Homam Nasser <homam.naser.3@gmail.com>
2026-02-05 17:53:22 +0300

    push

--- obj/Debug/net8.0/SGCP.AssemblyInfo.cs
+++ obj/Debug/net8.0/SGCP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGCP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48607f7acb1e2d25ef1554cea9bc5f2277c3655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3429eb5cf16656ef435d5fe864687d2a7a3f850d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGCP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGCP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
